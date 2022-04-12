850692ba8394ab0e2e0ad1ac94559c41ea3f4dd3
thebeardlessdwarf <vinivss@hotmail.com>
2022-04-11 23:13:36 -0700

    mostly done

---
+++ rocket/src/main.js
@@ -0,0 +1,14 @@
+let config = 
+{
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+let keyF, keyR, keyLEFT, keyRIGHT;
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
