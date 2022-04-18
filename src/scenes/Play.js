0ab1eb655dfeed75c92522126f33a48b9478a335
thebeardlessdwarf <vinivss@hotmail.com>
2022-04-18 14:30:38 -0700

    changes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,7 @@ class Play extends Phaser.Scene
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+        this.load.image('FastSmall', './assets/FastSmall.png');
         // load spritesheet
        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
       }
@@ -29,9 +30,10 @@ class Play extends Phaser.Scene
     
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
-        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 10).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 10).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+        this.fastShip = new FastSpaceship(this, game.config.width + borderUISize*9, borderUISize* 4, 'FastSmall', 0, 50).setOrigin(0,0);
          // define keys
          keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
          keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
