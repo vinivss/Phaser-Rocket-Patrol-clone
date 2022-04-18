6b3e8db093343b0ca2712d35dfdaa47ce3ca753e
thebeardlessdwarf <vinivss@hotmail.com>
2022-04-18 14:43:36 -0700

    asdas

--- src/prefabs/FastSpaceShip.js
+++ src/prefabs/FastSpaceShip.js
@@ -4,6 +4,9 @@ class FastSpaceship extends Phaser.GameObjects.Sprite
     constructor(scene, x, y, texture, frame, pointValue)
     {
         super(scene, x, y, texture, frame);
+
+
+        
         scene.add.existing(this);
         this.points = pointValue;
         this.moveSpeed = (game.settings.spaceshipSpeed * 2);
